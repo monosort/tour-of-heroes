74f8766b3250b5cde5c7835fbc423b6eb569debb
Henrique <henrique.munha@hotmail.com>
2025-07-03 15:21:38 +0100

    Working LIB

---
+++ data-access/src/lib/hero-api.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroApiService {
+  constructor() {}
+
+  sayHello() {
+    console.log('HeroApiService is working!');
+  }
+}
